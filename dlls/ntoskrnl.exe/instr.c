0a67b334378d7cf24ed6b024bad1ba9276fd5b92
Derek Lesho <dlesho@codeweavers.com>
2020-07-13 12:51:16 -0500

    ntoskrnl.exe: Emulate access to userspace IRP buffers.

--- dlls/ntoskrnl.exe/instr.c
+++ dlls/ntoskrnl.exe/instr.c
@@ -567,9 +567,39 @@ static DWORD64 current_rip;
 int read_emulated_memory(void *buf, BYTE *addr, unsigned int length)
 {
     SIZE_T offset;
+    struct kernel_struct *kernel_struct;
+    struct _KTHREAD *current_thread;
+    struct _EPROCESS *current_process = NULL;
 
     TRACE("(%p, %u)\n", addr, length);
 
+    if ((current_thread = TO_USER(NtCurrentTeb()->SystemReserved1[15])))
+    {
+        current_process = TO_USER(current_thread->process);
+
+        if (current_thread->user_input_copy)
+        {
+            ULONG size = HeapSize( GetProcessHeap(), 0, current_thread->user_input_copy );
+            offset = addr - current_thread->user_input;
+            if (size != -1 && offset + length <= size)
+            {
+                memcpy(buf, (BYTE*)current_thread->user_input_copy + offset, length);
+                goto done;
+            }
+        }
+        if (current_thread->user_output_copy)
+        {
+            ULONG size = HeapSize( GetProcessHeap(), 0, current_thread->user_output_copy );
+            offset = addr - current_thread->user_output;
+            if (size != -1 && offset + length <= size)
+            {
+                memcpy(buf, (BYTE*)current_thread->user_output_copy + offset, length);
+                goto done;
+            }
+        }
+    }
+
+    /* first check user shared data */
     offset = addr - user_shared_data;
     if (offset + length <= sizeof(KSHARED_USER_DATA))
     {
@@ -580,6 +610,72 @@ int read_emulated_memory(void *buf, BYTE *addr, unsigned int length)
 
     ERR("Failed to emulate memory access to %p+%u from %016llx\n", addr, length, current_rip);
     return 0;
+    done:
+    return 1;
+}
+
+int write_emulated_memory(BYTE *addr, void *buf, unsigned int length)
+{
+    SIZE_T offset;
+    struct _KTHREAD *current_thread;
+    struct _EPROCESS *current_process = NULL;
+    struct kernel_struct *kernel_struct;
+
+    TRACE("(%p, %u)\n", addr, length);
+
+    if ((current_thread = TO_USER(NtCurrentTeb()->SystemReserved1[15])))
+    {
+        current_process = TO_USER(current_thread->process);
+        if (current_thread->user_output_copy)
+        {
+            ULONG size = HeapSize( GetProcessHeap(), 0, current_thread->user_output_copy );
+            offset = addr - current_thread->user_output;
+            if (length == -1 && buf == addr && addr == current_thread->user_output)
+                length = size;
+            if (size != -1 && offset + length <= size)
+            {
+                memcpy((BYTE*)current_thread->user_output_copy + offset, buf, length);
+                goto done;
+            }
+        }
+        if (current_thread->user_input_copy)
+        {
+            ULONG size = HeapSize( GetProcessHeap(), 0, current_thread->user_input_copy );
+            offset = addr - current_thread->user_input;
+            if (length == -1 && buf == addr && addr == current_thread->user_input)
+                length = size;
+            if (size != -1 && offset + length <= size)
+            {
+                FIXME("ignoring write to input IRP memory\n");
+                memcpy((BYTE*)current_thread->user_input_copy + offset, buf, length);
+                goto done;
+            }
+        }
+    }
+
+    offset = addr - user_shared_data;
+    if (offset + length <= sizeof(KSHARED_USER_DATA))
+    {
+        FIXME("Writing to KSHARED_USER_DATA unsupported!\n");
+        goto fail;
+    }
+
+    if ((kernel_struct = get_kernel_struct(addr)))
+    {
+        offset = addr - kernel_struct->base;
+        if (offset + length <= kernel_struct->size)
+        {
+            if (kernel_struct->callback)
+                kernel_struct->callback(TO_USER(kernel_struct->base), offset, 1, (void *)current_rip);
+            memcpy(TO_USER(kernel_struct->base) + offset, buf, length);
+            return 1;
+        }
+    }
+
+    ERR("Failed to emulate memory access to %p+%u\n", addr, length);
+    return 0;
+    done:
+    return 1;
 }
 
 #define REX_B   1
